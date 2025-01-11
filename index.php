0967d49fc91f0a15e94418dc13b4440dce7e396e
Keys <keys14052002@gmail.com>
2025-01-11 05:29:42 +0000

    Remove team.php file

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
                 <li><a class='active' href='index.php?page=home'>Home</a>|</li>
                 <li><a href='index.php?page=about'>About</a>|</li>
                 <li><a href='index.php?page=contact'>Contact</a>|</li>
-                <li><a href='index.php?page=team'>Team</a>|</li>
                 <li><a href='index.php?page=projects'>Projects</a>|</li>                            
             </ul>
         </nav>
